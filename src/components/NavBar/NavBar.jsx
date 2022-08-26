3fed633bb8f4b8e277991079f686b0862de51cfd
spencer-dye <101621857+spencer-dye@users.noreply.github.com>
2022-08-26 14:31:18 -0600

    made changes to mobile navigation

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,21 +2,36 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import bdtLogo from '../../images/icons/bdt-logo-official.svg'
 import './NavBar.css'
-import mobileMenu from '../../images/icons/mobileMenu.svg'
+
+const primaryNav = document.querySelector('.primaryNavigation')
+const navToggle = document.querySelector('.mobileNavToggle')
+
+const handleClick = () => {
+    const visibility = primaryNav.getAttribute('data-visible')
+    if (visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true)
+        navToggle.setAttribute('aria-expanded', true)
+    } else {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', false)
+    }
+}
 
 const NavBar = () => {
   return (
     <nav>
         <div className="container navContainer">
             <img src={bdtLogo} alt="Benefits Data Trust Main Logo" className='logo'/>
-            <img src={mobileMenu} alt="mobile menu icon" className='mobileMenu'/>
-            <div className="linksWrap">
-                <Link to='/' className='navLink'>Who We Are</Link>
-                <Link to='/' className='navLink'>What We Do</Link>
-                <Link to='/' className='navLink'>Get Help</Link>
-                <Link to='/' className='navLink'>News & Insights</Link>
+            <button className='mobileNavToggle' aria-controls='primary-navigation' aria-expanded='false' onClick={handleClick}>
+                <span className='sr-only'>Menu</span>
+            </button>
+            <ul className="primaryNavigation" id='primary-navigation' data-visible='false'>
+                <li><Link to='/' className='navLink'>Who We Are</Link></li>
+                <li><Link to='/' className='navLink'>What We Do</Link></li>
+                <li><Link to='/' className='navLink'>Get Help</Link></li>
+                <li><Link to='/' className='navLink'>News & Insights</Link></li>
                 <input type="search" className='navSearch' placeholder='Search'/>
-            </div>
+            </ul>
         </div>
     </nav>
   )
