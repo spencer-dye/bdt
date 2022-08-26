a76f8b7a52dbaeb2797db401e9e8975bd4c212de
spencer-dye <101621857+spencer-dye@users.noreply.github.com>
2022-08-26 16:22:00 -0600

    Changes to mobile navigation

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import bdtLogo from '../../images/icons/bdt-logo-official.svg'
 import './NavBar.css'
 
-const primaryNav = document.querySelector('.primaryNavigation')
-const navToggle = document.querySelector('.mobileNavToggle')
+const NavBar = () => {
+
+const primaryNavRef = useRef(null)
+const navToggleRef = useRef(null)
 
 const handleClick = () => {
-    const visibility = primaryNav.getAttribute('data-visible')
+    const visibility = primaryNavRef.current.getAttribute('data-visible')
     if (visibility === 'false') {
-        primaryNav.setAttribute('data-visible', true)
-        navToggle.setAttribute('aria-expanded', true)
+        primaryNavRef.current.setAttribute('data-visible', true)
+        navToggleRef.current.setAttribute('aria-expanded', true)
     } else {
-        primaryNav.setAttribute('data-visible', false)
-        navToggle.setAttribute('aria-expanded', false)
+        primaryNavRef.current.setAttribute('data-visible', false)
+        navToggleRef.current.setAttribute('aria-expanded', false)
     }
 }
 
-const NavBar = () => {
   return (
     <nav>
         <div className="container navContainer">
             <img src={bdtLogo} alt="Benefits Data Trust Main Logo" className='logo'/>
-            <button className='mobileNavToggle' aria-controls='primary-navigation' aria-expanded='false' onClick={handleClick}>
+            <button className='mobileNavToggle' aria-controls='primary-navigation' aria-expanded='false' onClick={handleClick} ref={navToggleRef}>
                 <span className='sr-only'>Menu</span>
             </button>
-            <ul className="primaryNavigation" id='primary-navigation' data-visible='false'>
+            <ul className="primaryNavigation" id='primary-navigation' data-visible='false' ref={primaryNavRef}>
                 <li><Link to='/' className='navLink'>Who We Are</Link></li>
                 <li><Link to='/' className='navLink'>What We Do</Link></li>
                 <li><Link to='/' className='navLink'>Get Help</Link></li>
